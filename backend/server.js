0a790b3e22a5b73c78180cb86ab9b0491fb45b6f
pranabgrg17 <pranabgurung2735@gmail.com>
2025-04-12 21:46:36 +0545

    woooo

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoutes.js";
 import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js";
 
 // App Config
 const app = express();
@@ -20,6 +21,7 @@ connectDB();
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))//to access uploaded image to frontend
 app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
 
 
 app.get("/", (req, res) => {
